db255cff1d72d7dd5fd9778c8651bbdeb4453f21
foleq <piotrekgda90@gmail.com>
2018-03-16 22:31:50 +0100

    http request - more observable and using flatMap

--- main.ts
+++ main.ts
@@ -6,23 +6,37 @@ let button = document.getElementById("button");
 let click = Observable.fromEvent(button, "click");
 
 function load(url: string) {
-    let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener("load", () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach(m => {
-            let div = document.createElement("div");
-            div.innerText = m.title;
-            output.appendChild(div);
+    return Observable.create(observer => {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
         });
-    });
 
-    xhr.open("GET", url);
-    xhr.send();
+        xhr.open("GET", url);
+        xhr.send();
+    });
 }
 
-click.subscribe(
-    e => load("movies.json"),
-    e => console.log(`error: ${e}`),
-    () => console.log("complete")
-);
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+//this code doesn't request server for movies.json until someone will subscribe to it
+load("movies.json");
+
+// with map we'll get stream of 'streams' instead of flatMap give us stream of movies
+click.flatMap(e => load("movies.json"))
+    .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+    );
