db7b80112594c9168607803a2f735edc64d8632e
foleq <piotrekgda90@gmail.com>
2018-03-16 20:07:17 +0100

    Easier Observer (without creating new class)

--- main.ts
+++ main.ts
@@ -1,21 +1,10 @@
-import { Observable, Observer } from "rxjs";
+import { Observable } from "rxjs";
 
 let numbers = [1, 5, 10];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
-    next(value: number){
-        console.log(`value: ${value}`);
-    }
-
-    error(e){
-        console.log(`error: ${e}`);
-    }
-
-    complete(){
-        console.log("complete");
-    }
-}
-
-source.subscribe(new MyObserver());
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+);
