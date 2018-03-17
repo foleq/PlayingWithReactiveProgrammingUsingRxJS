d0a61c198931a2367ce9155cc647121eaddae65e
foleq <piotrekgda90@gmail.com>
2018-03-17 11:45:16 +0100

    retryWhen strategy to throw an error after all attempts failed

--- loader.ts
+++ loader.ts
@@ -33,18 +33,21 @@ export function loadWithFetch(url: string) {
                 } else {
                     return Promise.reject(response);
                 }
-            }));
-    });
+            }))
+    }).retryWhen(retryStrategy());
 }
 
-function retryStrategy({ attempts = 4, delay = 1000 }) {
+function retryStrategy({ attempts = 4, delay = 1000 } = {}) {
     return function(errors) {
         return errors
             .scan((acc, value) => {
-                console.log(acc, value)
-                return acc + 1;
+                acc += 1;
+                if(acc < attempts) {
+                    return acc;
+                } else {
+                    throw new Error(value);
+                }
             }, 0)
-            .takeWhile(acc => acc < attempts)
             .delay(delay);
     }
 }
